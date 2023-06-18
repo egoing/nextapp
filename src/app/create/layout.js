0a316f1e31a52729607bc190d5c5e65483a71e20
egoing <egoing@gmail.com>
2023-06-18 17:34:01 +0900

    생성 기능 구현
    https://opentutorials.org/module/6341/32357

--- src/app/create/layout.js
+++
@@ -1,6 +0,0 @@
-export default function Layout(props){
-  return <form>
-    <h2>Create</h2>
-    {props.children}
-  </form>
-}
--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,5 +1,28 @@
+'use client'
+ 
+import { useRouter } from "next/navigation";
+ 
 export default function Create(){
-  return <>
-    Create!!
-  </>
+  const router = useRouter();
+  return <form onSubmit={async evt=>{
+    evt.preventDefault();
+    const title = evt.target.title.value;
+    const body = evt.target.body.value;
+    const resp = await fetch('http://localhost:9999/topics/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({title, body})
+    });
+    const topic = await resp.json();
+    console.log("file: page.js:19 ~ Create ~ topic:", topic)
+    router.push(`/read/${topic.id}`);
+    router.refresh();
+  }}>
+    <h2>Create</h2>
+    <p><input type="text" name="title" placeholder="title" /></p>
+    <p><textarea name="body" placeholder="body"></textarea></p>
+    <p><input type="submit" value="create" /></p>
+  </form>
 }
