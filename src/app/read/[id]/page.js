c5f80f61d9e279f3a55f07c026a1f7d8c2e17421
egoing <egoing@gmail.com>
2023-06-18 17:26:15 +0900

    읽기 기능 구현
    https://opentutorials.org/module/6341/32356

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,6 +1,9 @@
-export default function Read(props){
+export default async function Read(props){
+  const id = props.params.id;
+  const resp = await fetch(`http://localhost:9999/topics/${id}`);
+  const topic = await resp.json();
   return <>
-    <h2>Read</h2>
-    parameter : {props.params.id}
+    <h2>{topic.title}</h2>
+    {topic.body}
   </>
 }
