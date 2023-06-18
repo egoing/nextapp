3938ee91148d7bd8096211866e4cc26e3f64662b
egoing <egoing@gmail.com>
2023-06-18 17:58:18 +0900

    수정 기능 구현하기 https://opentutorials.org/module/6341/32359

---
+++ src/app/update/[id]/page.js
@@ -0,0 +1,39 @@
+'use client'
+import { useRouter } from "next/navigation";
+import {useEffect, useState} from 'react';
+ 
+export default function Update(props){
+  const router = useRouter();
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const id = props.params.id;
+  async function refresh(){
+    const resp = await fetch(`http://localhost:9999/topics/${id}`);
+    const topic = await resp.json();
+    setTitle(topic.title);
+    setBody(topic.body);
+  }
+  useEffect(()=>{
+    refresh();
+  }, []);
+  return <form onSubmit={async evt=>{
+    evt.preventDefault();
+    const title = evt.target.title.value;
+    const body = evt.target.body.value;
+    const resp = await fetch(`http://localhost:9999/topics/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({title, body})
+    });
+    const topic = await resp.json();
+    router.push(`/read/${topic.id}`);
+    router.refresh();
+  }}>
+    <h2>Update</h2>
+    <p><input type="text" name="title" placeholder="title" onChange={e=>setTitle(e.target.value)} value={title}/></p>
+    <p><textarea name="body" palceholder="body" onChange={e=>setBody(e.target.value)} value={body}></textarea></p>
+    <p><input type="submit" value="update" /></p>
+  </form>
+}
